5bcd75266fa12e48de20a81b45f2090b629fade4
abdullahimunye <amunye1@ggc.edu>
2023-11-05 16:17:23 -0500

    added the search functionality to the home page

--- app/src/main/java/dev/spikeysanju/wiggles/view/Home.kt
+++ app/src/main/java/dev/spikeysanju/wiggles/view/Home.kt
@@ -15,20 +15,37 @@
  */
 package dev.spikeysanju.wiggles.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import dev.spikeysanju.wiggles.R
 import dev.spikeysanju.wiggles.component.ItemDogCard
 import dev.spikeysanju.wiggles.component.TopBar
 import dev.spikeysanju.wiggles.model.Dog
 
 @Composable
 fun Home(navController: NavHostController, dogList: List<Dog>, toggleTheme: () -> Unit) {
+    // State to hold the gender filter text
+    var genderFilter by remember { mutableStateOf("") }
     LazyColumn {
         item {
             TopBar(
@@ -37,15 +54,43 @@ fun Home(navController: NavHostController, dogList: List<Dog>, toggleTheme: () -
                 }
             )
             Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = genderFilter,
+                onValueChange = { genderFilter = it },
+                label = { Text("Enter gender") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White, RoundedCornerShape(4.dp)), // Add background color and shape
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+
+                        painter= painterResource(id = R.drawable.ic_filled_search),
+
+                        contentDescription = "A magnify glass to search",
+                        tint = Color.Black
+                    )
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    backgroundColor = Color.White,
+                    cursorColor = Color.Black,
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                )
+            )
+
         }
         items(dogList) {
-            dogList.forEach {
-                ItemDogCard(
-                    it,
-                    onItemClicked = { dog ->
-                        navController.navigate("details/${dog.id}/${dog.name}/${dog.location}")
-                    }
-                )
+
+                dogList.forEach {
+                    if (genderFilter.isBlank() || it.gender.equals(genderFilter, ignoreCase = true) || it.name.equals(genderFilter, ignoreCase = true)) {
+                    ItemDogCard(
+                        it,
+                        onItemClicked = { dog ->
+                            navController.navigate("details/${dog.id}/${dog.name}/${dog.location}")
+                        }
+                    )
+                }
             }
         }
     }
